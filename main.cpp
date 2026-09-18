d8902a93e83aec7e07404fa2c7d20b87b108200e
arthur-og <arthur12.2007g@gmail.com>
2026-09-17 22:51:14 -0300

    add simple optimizer

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <iterator>
 #include <vector>
 using namespace std;
-const float der = 1e-4f;
+const float h = 1e-4f;
 vector<float> data_tr;
 class Train {
     const int epochs = 10000;
@@ -14,35 +14,42 @@ class Train {
     }
 
   public:
-    float cost(float w, float b) {
+    float cost(vector<float> &params) {
         float result = 0;
         for (int i = 0; i < data_tr.size(); ++i) {
             float x = data_tr[i];
             float y = oracle(x);
-            float p = w * x + b;
+            float p = params[0] * x + params[1];
             float d = y - p;
             result += d * d;
-            // cout << w << " " << oracle(train_tr[i]) << '\n';
         }
         return result / data_tr.size();
     }
-    vector<float> derivate(float w, float b) {
-        return {(cost(w + der, b) - cost(w - der, b)) / (2.0f * der),
-                (cost(w, b + der) - cost(w, b - der)) / (2.0f * der)};
+    vector<float> optimizer(vector<float> params) {
+        vector<float> derivates(params.size());
+        for (int i = 0; i < params.size(); ++i) {
+            float temp = params[i];
+            params[i] = temp + h;
+            float costp = cost(params);
+            params[i] = temp - h;
+            float costm = cost(params);
+            derivates[i] = (costp - costm) / (2 * h);
+            params[i] = temp;
+        }
+        return derivates;
     }
     vector<float> loop() {
         srand(time(nullptr));
-        float w = 10.0f * (float)rand() / RAND_MAX;
-        float b = 5.0f * (float)rand() / RAND_MAX;
+        vector<float> params = {10.0f * (float)rand() / RAND_MAX, 5.0f * (float)rand() / RAND_MAX};
         for (int i = 0; i < epochs; ++i) {
 
-            cout << " w: " << w << " b: " << b << " cost: " << cost(w, b) << endl;
-            vector<float> grad = derivate(w, b);
-            w = w - lr * grad[0];
-            b = b - lr * grad[1];
+            cout << " w: " << params[0] << " b: " << params[1] << " cost: " << cost(params) << endl;
+            vector<float> grad = optimizer(params);
+            params[0] = params[0] - lr * grad[0];
+            params[1] = params[1] - lr * grad[1];
         }
         cout << endl;
-        return {w, b};
+        return params;
     }
 };
 
