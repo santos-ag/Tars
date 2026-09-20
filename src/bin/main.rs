d6453611cf68e56756217a38d1f277c6fd63a9cd
arthur-og <arthur12.2007g@gmail.com>
2026-09-20 14:44:48 -0300

    functions used from the lib in main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,12 @@
+use tars::{cost,optimizer};
 const DATA_TR:[[f32;2];5] = [[1.0,2.0],[2.0,4.0],[5.0,10.0],[0.0,0.0],[-2.0,-4.0]];
-fn cost(w:f32)->f32{
-    let mut loss = 0.0;
-    for [x,y] in DATA_TR{
-        let erro = y-x * w;
-        loss += erro*erro;
-    }
-    loss
-}
-fn num_grad(w:f32)->f32{
-    let h = 1e-4;
-    (cost(w+h)-cost(w-h))/(2.0*h)
-}
-fn optimizer(w:f32,lr:f32)->f32{
-    w - num_grad(w) * lr
-}
+
 fn main() {
     const LR:f32 = 1e-2;
     let mut w:f32 = rand::random();
-    println!("cost is:{}",cost(w));
+    println!("cost is:{}",cost(w,&DATA_TR));
     for _ in 0..15{
-        w = optimizer(w,LR);
-        println!("cost is:{}",cost(w));
+        w = optimizer(w,LR,&DATA_TR);
+        println!("cost is:{}",cost(w,&DATA_TR));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
-pub fn add(w:f32) -> f32 {
-   return w; 
+pub fn cost(w:f32,data:&[[f32;2]])->f32{
+    let mut loss = 0.0;
+    for [x,y] in data{
+        let erro = y-x * w;
+        loss += erro*erro;
+    }
+    loss
+}
+pub fn num_grad(w:f32,data:&[[f32;2]])->f32{
+    let h = 1e-4;
+    (cost(w+h,&data)-cost(w-h,&data))/(2.0*h)
+}
+pub fn optimizer(w:f32,lr:f32,data:&[[f32;2]])->f32{
+    w - num_grad(w,&data) * lr
 }
 
 
