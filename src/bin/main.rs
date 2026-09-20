c718637748787d21113940f051ac87196f8aeb77
arthur-og <arthur12.2007g@gmail.com>
2026-09-20 16:06:10 -0300

    beat ax+b and improve visualization

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,25 @@
 use tars::*;
-const DATA_TR:[[f32;2];5] = [[1.0,2.0],[2.0,4.0],[5.0,10.0],[0.0,0.0],[-2.0,-4.0]];
+const DATA_TR:[[f32;2];5] = [[1.0,15.0],[2.0,47.0],[5.0,143.0],[0.0,-17.0],[-2.0,-81.0]];
 
 fn main() {
     const LR:f32 = 1e-2;
+    const EPOCHS:usize = 150;
     let mut w:f32 = rand::random();
-    println!("cost is:{}",cost(w,&DATA_TR));
-    for _ in 0..15{
-        w = optimizer(w,LR,&DATA_TR);
-        println!("cost is:{}",cost(w,&DATA_TR));
+    let mut b:f32 = rand::random();
+    w=w*5.0-1.0;
+    b = b*5.0-1.0;
+    let mut prev_cost = cost(w,b,&DATA_TR);
+            println!("epoch: 000000, cost is:{:014.8}",prev_cost);
+    {
+    for i in 1..EPOCHS{
+        [w,b] = optimizer(w,b,LR,&DATA_TR);
+        let curr_cost = cost(w,b,&DATA_TR);
+        if i%(EPOCHS/20)==0 {
+            println!("epoch: {:06.0}, cost is:{:014.8}, {:07.3}% better",i,curr_cost,((prev_cost-curr_cost)*100.0)/prev_cost);
+
+        }
+        prev_cost = curr_cost;
     }
+    }
+    println!("w is : {w},b is {b}");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,26 @@ pub struct Data<const IN: usize,const OUT:usize>{
     pub target: [f32;OUT]
 }
 
-pub fn cost(w:f32,data:&[[f32;2]])->f32{
+pub fn cost(w:f32,b:f32,data:&[[f32;2]])->f32{
     let mut loss = 0.0;
     for [x,y] in data{
-        let erro = y-x * w;
+        let erro = y-(x*w+b);
         loss += erro*erro;
     }
     loss
 }
-pub fn num_grad(w:f32,data:&[[f32;2]])->f32{
-    let h = 1e-4;
-    (cost(w+h,&data)-cost(w-h,&data))/(2.0*h)
+pub fn num_grad(w:f32,b:f32,data:&[[f32;2]])->[f32;2]{
+    let h = 1e-3;
+    [
+        (cost(w+h,b,&data)-cost(w-h,b,&data))/(2.0*h),
+        (cost(w,b+h,&data)-cost(w,b-h,&data))/(2.0*h),
+    ]
 }
-pub fn optimizer(w:f32,lr:f32,data:&[[f32;2]])->f32{
-    w - num_grad(w,&data) * lr
+pub fn optimizer(w:f32,b:f32,lr:f32,data:&[[f32;2]])->[f32;2]{
+    [
+        w - num_grad(w,b,&data)[0] * lr,
+        b - num_grad(w,b,&data)[1]*lr
+    ]
 }
 
 
