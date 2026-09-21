29932fc1c60a85d34525dd9fa1933cad2c98161e
arthur-og <arthur12.2007g@gmail.com>
2026-09-20 21:39:00 -0300

    add structs and builders for Model,Data,Grad,Optimizer,also add sigmoidf and forward

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,19 +1,29 @@
 use tars::*;
-const DATA_TR:[[f32;2];5] = [[1.0,15.0],[2.0,47.0],[5.0,143.0],[0.0,-17.0],[-2.0,-81.0]];
+const DATA_TR:[Data;4] = [
+   Data::new([0.0,0.0],[0.0]),
+   Data::new([1.0,0.0],[1.0]),
+   Data::new([0.0,1.0],[1.0]),
+   Data::new([1.0,1.0],[1.0])
+];
 
 fn main() {
-    const LR:f32 = 1e-2;
-    const EPOCHS:usize = 150;
-    let mut w:f32 = rand::random();
-    let mut b:f32 = rand::random();
-    w=w*5.0-1.0;
-    b = b*5.0-1.0;
-    let mut prev_cost = cost(w,b,&DATA_TR);
+    const LR:f32 = 1e1;
+    const EPOCHS:usize = 100000;
+    let mut model=Model{
+        weights:[
+            rand::random(),
+            rand::random()
+        ],
+        bias:[rand::random();1]
+    };
+    let optimizer = BGD::new(LR);
+    let mut prev_cost = cost(&model,&DATA_TR);
             println!("epoch: 000000, cost is:{:014.8}",prev_cost);
     {
-    for i in 1..EPOCHS{
-        [w,b] = optimizer(w,b,LR,&DATA_TR);
-        let curr_cost = cost(w,b,&DATA_TR);
+    for i in 1..EPOCHS+1{
+        let grad = num_grad(&model,&DATA_TR);
+        optimizer.step(&mut model,&grad);
+        let curr_cost = cost(&model,&DATA_TR);
         if i%(EPOCHS/20)==0 {
             println!("epoch: {:06.0}, cost is:{:014.8}, {:07.3}% better",i,curr_cost,((prev_cost-curr_cost)*100.0)/prev_cost);
             prev_cost = curr_cost;
@@ -21,5 +31,5 @@ fn main() {
         }
     }
     }
-    println!("w is : {w},b is {b}");
+    println!("The model is: {:?}",model);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,107 @@
 #[derive(Clone,Copy,Debug)]
-pub struct Data<const IN: usize,const OUT:usize>{
-    pub input: [f32;IN],
-    pub target: [f32;OUT]
+pub struct Data{
+    pub input: [f32;2],
+    pub target: [f32;1]
+}
+impl Data {
+    pub const fn new(input:[f32;2],target:[f32;1])->Self{
+        Self {
+            input,
+            target
+        }
+    }
 }
 
-pub fn cost(w:f32,b:f32,data:&[[f32;2]])->f32{
+#[derive(Clone,Copy,Debug)]
+pub struct Grad{
+    pub weights:[f32;2],
+    pub bias:[f32;1]
+}
+
+#[derive(Clone,Copy,Debug)]
+pub struct Model{
+    pub weights:[f32;2],
+    pub bias:[f32;1]
+}
+
+impl Model{
+    pub fn new(weights:[f32;2],bias:[f32;1])->Self{
+        Self{
+            weights,
+            bias
+        }
+    }
+}
+
+#[derive(Clone,Copy,Debug)]
+pub struct BGD{
+    lr:f32
+}
+
+impl BGD{
+    pub fn new(lr:f32)->Self{
+        Self{
+            lr
+        }
+    }
+    pub fn step(&self,model:&mut Model,grad:&Grad){
+        for i in 0..model.weights.len(){
+            model.weights[i] -= grad.weights[i] * self.lr;
+
+        }
+        for i in 0..model.bias.len(){
+            model.bias[i] -= grad.bias[i] * self.lr;
+
+        }    
+    }
+}
+pub fn sigmoidf(x:f32)->f32{
+    1.0/(1.0+(-x).exp())
+}
+pub fn forward(model: &Model,input:[f32;2])->f32{
+    let mut z = model.bias[0];
+    for i in 0..model.weights.len(){
+        z+=input[i]*model.weights[i];
+    }
+    sigmoidf(z)
+}
+pub fn cost(model:&Model,data:&[Data;4])->f32{
     let mut loss = 0.0;
-    for [x,y] in data{
-        let erro = y-(x*w+b);
+    for sample in data{
+        let erro = sample.target[0]-forward(model,sample.input);
         loss += erro*erro;
     }
     loss
 }
-pub fn num_grad(w:f32,b:f32,data:&[[f32;2]])->[f32;2]{
+pub fn num_grad(model: &Model,data:&[Data;4])->Grad{
+    let mut tempM = *model;
+
+    let mut grad=Grad{
+        weights:[0.0;2],
+        bias:[0.0;1]
+    };
+
     let h = 1e-3;
-    [
-        (cost(w+h,b,&data)-cost(w-h,b,&data))/(2.0*h),
-        (cost(w,b+h,&data)-cost(w,b-h,&data))/(2.0*h),
-    ]
+
+    for i in 0..model.weights.len(){
+        tempM.weights[i] += h;
+        let costp = cost(&tempM,data);
+        tempM.weights[i] -= 2.0*h;
+        let costm = cost(&tempM,data);
+        grad.weights[i] = (costp-costm)/(2.0*h);
+        tempM.weights[i] += h;
+    }
+    for i in 0..model.bias.len(){
+        tempM.bias[i] += h;
+        let costp = cost(&tempM,data);
+        tempM.bias[i] -= 2.0*h;
+        let costm = cost(&tempM,data);
+        grad.bias[i] = (costp-costm)/(2.0*h);
+        tempM.bias[i] += h;
+    }
+    grad
+
 }
-pub fn optimizer(w:f32,b:f32,lr:f32,data:&[[f32;2]])->[f32;2]{
-    [
-        w - num_grad(w,b,&data)[0] * lr,
-        b - num_grad(w,b,&data)[1]*lr
-    ]
-}
+
 
 
