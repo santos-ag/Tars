bfe65ba4d5b93cf1b73854042573f2426eb76b94
arthur-og <arthur12.2007g@gmail.com>
2026-09-24 19:35:52 -0300

    Match each type of anymodule and use as_linear and as_mut_linear for acess linear in AnyModule

--- src/grad.rs
+++ src/grad.rs
@@ -1,38 +1,44 @@
+use crate::AnyModule;
 use crate::Data;
-use crate::Layer;
-use crate::Model;
+use crate::Linear;
+use crate::Sequential;
 use crate::cost;
 #[derive(Clone, Debug)]
 pub struct Grad {
-    pub layers: Vec<Layer>,
+    pub modules: Vec<AnyModule>,
 }
 
-pub fn num_grad<const IN: usize, const OUT: usize>(model: &Model, data: &[Data<IN, OUT>]) -> Grad {
-    let mut temp_m = model.clone();
-
+pub fn num_grad<const IN: usize, const OUT: usize>(
+    model: &Sequential,
+    data: &[Data<IN, OUT>],
+) -> Grad {
+    let mut temp_model = model.clone();
     let mut grad = Grad {
-        layers: model.layers.clone(),
+        modules: model.modules.clone(),
     };
-
     let h = 1e-3;
-    for l in 0..model.layers.len() {
-        for o in 0..model.layers[l].weights.len() {
-            for w in 0..model.layers[l].weights[o].len() {
-                temp_m.layers[l].weights[o][w] += h;
-                let costp = cost(&temp_m, data);
-                temp_m.layers[l].weights[o][w] -= 2.0 * h;
-                let costm = cost(&temp_m, data);
-                grad.layers[l].weights[o][w] = (costp - costm) / (2.0 * h);
-                temp_m.layers[l].weights[o][w] += h;
+    for m in 0..model.modules.len() {
+        match &model.modules[m] {
+            AnyModule::Linear(linear) => {
+                for o in 0..linear.out_sz {
+                    for w in 0..linear.in_sz {
+                        temp_model.modules[m].as_mut_linear().weights[o][w] += h;
+                        let costp = cost(&temp_model, data);
+                        temp_model.modules[m].as_mut_linear().weights[o][w] -= 2.0 * h;
+                        let costm = cost(&temp_model, data);
+                        grad.modules[m].as_mut_linear().weights[o][w] = (costp - costm) / (2.0 * h);
+                        temp_model.modules[m].as_mut_linear().weights[o][w] += h;
+                    }
+
+                    temp_model.modules[m].as_mut_linear().bias[o] += h;
+                    let costp = cost(&temp_model, data);
+                    temp_model.modules[m].as_mut_linear().bias[o] -= 2.0 * h;
+                    let costm = cost(&temp_model, data);
+                    grad.modules[m].as_mut_linear().bias[o] = (costp - costm) / (2.0 * h);
+                    temp_model.modules[m].as_mut_linear().bias[o] += h;
+                }
             }
-        }
-        for o in 0..model.layers[l].bias.len() {
-            temp_m.layers[l].bias[o] += h;
-            let costp = cost(&temp_m, data);
-            temp_m.layers[l].bias[o] -= 2.0 * h;
-            let costm = cost(&temp_m, data);
-            grad.layers[l].bias[o] = (costp - costm) / (2.0 * h);
-            temp_m.layers[l].bias[o] += h;
+            AnyModule::Activation(_) => {}
         }
     }
     grad
